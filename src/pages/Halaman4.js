d5f07c520f9faaf7fdf0232580499e11ad026a83
Rayhan Nurcholis <rayhan.a.nurcholis@gmail.com>
2023-10-09 15:12:05 +0700

    animasi

--- src/pages/Halaman4.js
+++ src/pages/Halaman4.js
@@ -19,25 +19,25 @@ const Halaman4 = () => {
   return (
     <div className={`${theme}`}>
       <div className='h-screen bg-[#ffffff] bb dark:bg-black flex justify-center items-center'>
-        <button className='bg-green-200 p-4 rounded-3xl' onClick={()=>setTheme(theme==='dark'? 'light': 'dark')}>
+        <button className='p-4 bg-green-200 rounded-3xl' onClick={()=>setTheme(theme==='dark'? 'light': 'dark')}>
           Dark Mode
         </button>
       </div>
-    <div className='max-w-lg my-10 dark:bg-black  border-slate-200 rounded-xl  mx-auto p-5 shadow-md font-inter hover:bg-sky-500 group border-4 font-serif'>
-      <h5 className='font-bold text-slate-900 text-lg mb-3 group-hover:text-white selection:bg-red-400'>My Card</h5>
+    <div className='max-w-lg p-5 mx-auto my-10 font-serif transition border-4 shadow-md dark:bg-black border-slate-200 rounded-xl font-inter hover:bg-sky-500 group hover:scale-125'>
+      <h5 className='mb-3 text-lg font-bold text-slate-900 group-hover:text-white selection:bg-red-400'>My Card</h5>
       <p className='text-slate-600 group-hover:text-white selection:bg-red-400 first-letter:text-4xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus repellendus doloribus voluptatibus odit accusantium voluptate sed amet corporis tempora ducimus, laborum fuga doloremque velit quas harum quod quae exercitationem. Quae.</p>
     </div>
     <div>
-    <button className='my-10 bg-sky-800 px-5 py-2 rounded-full text-white font-semibold block mx-auto hover:bg-slate-500'>Save</button>
+    <button className='block px-5 py-2 mx-auto my-10 font-semibold text-white rounded-full bg-sky-800 hover:bg-slate-500'>Save</button>
     </div>
 
-    <div className='mb-6 max-w-lg  border-slate-200 rounded mx-auto shadow-md font-inter p-4 border-4'>
+    <div className='max-w-lg p-4 mx-auto mb-6 border-4 rounded shadow-md border-slate-200 font-inter'>
       <form action="" className=''>
-        <label htmlFor="email" className="block font-semibold mb-1 text-slate-700 after:text-pink-500">
+        <label htmlFor="email" className="block mb-1 font-semibold text-slate-700 after:text-pink-500">
         Email *
         </label>
-        <input type='email' id='email' placeholder='masukkan email...' className='px-3 py-2 border shadow rounded w-full block text-sm placeholder:text-slate-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:border-blue-400 invalid:text-pink-500 peer'/>
-        <p className='m-2 text-pink-700 invisible peer-invalid:visible'>Email tidak valid</p>
+        <input type='email' id='email' placeholder='masukkan email...' className='block w-full px-3 py-2 text-sm border rounded shadow placeholder:text-slate-500 focus:outline-none focus:ring-1 focus:ring-sky-500 focus:border-blue-400 invalid:text-pink-500 peer'/>
+        <p className='invisible m-2 text-pink-700 peer-invalid:visible'>Email tidak valid</p>
       </form>
 
     </div>
--- src/pages/Halaman5.js
+++ src/pages/Halaman5.js
@@ -3,10 +3,19 @@ import { Link } from 'react-router-dom'
 const Halaman5 = () => {
   return (
     <div>
-    <div>Halaman5</div>
-    <div className="flex flex-wrap">
-<Link to={'/halaman6'} className="p-2 border-2 border-black"> Halaman 6</Link>
-   </div>
+    <div className='w-40 h-40 mx-auto mt-20 transition rounded-lg shadow-lg hover:rotate-45 mt hover:scale-125 bg-sky-500 hover:bg-pink-700'>
+      
+    </div>
+    <div className='w-40 h-40 mx-auto mt-20 bg-pink-500 group'>
+      <div className='w-full h-full transition transition-transform duration-700 origin-top-left shadow-lg roundied-lg bg-sky-500 hover:rotate-45 group-hover:rotate-180'></div>
+    </div>
+    <div className='w-40 h-40 mx-auto mt-20 mb-20 transition rounded-lg shadow-lg bg-sky-500 animate-spin'>
+      
+    </div>
+    <div className='w-40 h-40 mx-auto mt-20 mb-20 transition rounded-full shadow-lg bg-sky-500 animate-bounce'>
+      
+    </div>
+   
     </div>
   )
 }
